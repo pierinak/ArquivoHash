5a17f2942b34abff97142960c754649f38944e58
Pierina <110428224+pierinak@users.noreply.github.com>
2024-05-21 22:19:05 -0400

    Update book.h

--- book.h
+++ book.h
@@ -1,33 +1,68 @@
-#ifndef BOOK_H
-#define BOOK_H
+#ifndef LIVROS_H
+#define LIVROS_H
 
-#include <string>
-#include <unordered_map>
-#include <vector>
 #include <iostream>
+#include <cstring>
+#define TABLE_SIZE 11
 
 using namespace std;
 
-struct Book {
-    string isbn;
-    string title;
-    string author;
-    int pages;
-
-    Book() = default;
-    Book(const string& isbn, const string& title, const string& author, int pages)
-        : isbn(isbn), title(title), author(author), pages(pages) {}
+struct Livro {
+    int isbn;
+    char titulo[100];
+    char autor[100];
+    int paginas;
 };
 
-class BookCollection {
-public:
-    void addBook(const Book& book);
-    void removeBook(const string& isbn);
-    Book* searchBook(const string& isbn);
-    void listBooks() const;
+int hashLivros(int isbn);
+void initLivros(Livro* livros);
+void insereLivro(Livro* livros, Livro livro, int index);
+Livro* buscarLivro(Livro* livros, int isbn);
+void removerLivro(Livro* livros, int isbn);
+void mostrarLivros(Livro* livros);
 
-private:
-    unordered_map<string, Book> books;
-};
+int hashLivros(int isbn) {
+    return isbn % TABLE_SIZE;
+}
 
-#endif // BOOK_H
+void initLivros(Livro* livros) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        livros[i].isbn = -1;
+        strcpy(livros[i].titulo, "");
+        strcpy(livros[i].autor, "");
+        livros[i].paginas = 0;
+    }
+}
+
+void insereLivro(Livro* livros, Livro livro, int index) {
+    livros[index] = livro;
+}
+
+Livro* buscarLivro(Livro* livros, int isbn) {
+    int index = hashLivros(isbn);
+    if (livros[index].isbn == isbn) {
+        return &livros[index];
+    }
+    return nullptr;
+}
+
+void removerLivro(Livro* livros, int isbn) {
+    int index = hashLivros(isbn);
+    if (livros[index].isbn == isbn) {
+        livros[index].isbn = -1;
+        strcpy(livros[index].titulo, "");
+        strcpy(livros[index].autor, "");
+        livros[index].paginas = 0;
+    }
+}
+
+void mostrarLivros(Livro* livros) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (livros[i].isbn != -1) {
+            cout << "ISBN: " << livros[i].isbn << ", Título: " << livros[i].titulo
+                 << ", Autor: " << livros[i].autor << ", Páginas: " << livros[i].paginas << endl;
+        }
+    }
+}
+
+#endif
